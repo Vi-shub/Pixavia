5793419153058ecf39af7909a56d29f6a8915028
Vi-shub <smsharma3121@gmail.com>
2025-01-09 14:25:25 +0530

    change-10

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -14,49 +14,8 @@ const PageBanner = ({ pageTitle, pageDesc }) => {
       </Head>
       {/* Onovo Intro */}
 			<section className="onovo-section onovo-intro intro--black">
-				<div className="container">
-					<h1 className="onovo-title-1  onovo-text-white">
-						<span>{pageTitle}</span>
-						<span className="onovo-sep word">
-							<i className="sep-img" style={{"backgroundImage": "url(/images/title_icon.svg)"}} />
-						</span>
-					</h1>
-					{pageDesc != "" &&
-					<div className="onovo-subtitle-2  onovo-text-white">
-						<span>{pageDesc}</span>
-					</div>
-					}
-					<div className="onovo-breadcrums">
-						<ul>
-							<li>
-								<Link href="/">Home</Link>
-							</li>
-							{asPath.indexOf('/blog/') != -1 &&
-							<li>
-								<Link href="/blog">Blog</Link>
-							</li>
-							}
-							{asPath.indexOf('/projects/') != -1 &&
-							<li>
-								<Link href="/projects">Projects</Link>
-							</li>
-							}
-							{asPath.indexOf('/services/') != -1 &&
-							<li>
-								<Link href="/services">Services</Link>
-							</li>
-							}
-							{asPath.indexOf('/team/') != -1 &&
-							<li>
-								<Link href="/team">Team</Link>
-							</li>
-							}
-							<li className="current">
-								<span>{pageTitle}</span>
-							</li>
-						</ul>
-					</div>
-				</div>
+				
+				
 			</section>
     </>
   );
