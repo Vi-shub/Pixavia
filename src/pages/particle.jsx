e73dd0ce4f3cf0f9df99e52e843c1421632b9d2a
Vi-shub <smsharma3121@gmail.com>
2025-01-23 16:05:38 +0530

    major-2

--- src/pages/particle.jsx
+++ src/pages/particle.jsx
@@ -5,18 +5,16 @@ const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false })
 
 export default function Particle() {
   return (
-    <main >
+    <main style={{ position: 'relative', width: '100%', height: '100%' }}>
       {/* Desktop view */}
       <Spline
-        scene="https://prod.spline.design/naeIOddvbnVnRfK6/scene.splinecode" 
-       
+        scene="https://prod.spline.design/naeIOddvbnVnRfK6/scene.splinecode"
         className="desktop-view"
       />
 
       {/* Mobile view */}
       <Spline
-       scene="https://prod.spline.design/vjWvcP1hcsjSz3WR/scene.splinecode" 
-        
+        scene="https://prod.spline.design/vjWvcP1hcsjSz3WR/scene.splinecode"
         className="mobile-view"
       />
 
@@ -24,15 +22,21 @@ export default function Particle() {
         {`
           /* Desktop view: Visible on screens 768px and above */
           @media (min-width: 768px) {
-            .desktop-view {
+            .mobile-view {
               display: none;
+            }
+            .desktop-view {
+              display: block;
             }
           }
 
           /* Mobile view: Visible on screens below 768px */
           @media (max-width: 767px) {
-            .mobile-view {
+            .desktop-view {
               display: none;
+            }
+            .mobile-view {
+              display: block;
             }
           }
         `}
@@ -40,4 +44,3 @@ export default function Particle() {
     </main>
   );
 }
-
